1f3191970fee96eab69e78f5ed203acf47b68c72
Elsa <ljq5152@gmail.com>
2022-05-23 13:24:39 +0930

    code

--- server.js
+++ server.js
@@ -12,25 +12,25 @@ app.use(express.static('public'));
 app.use(express.json());
 
 app.post('/api/notes', (req, res) => {
-  fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) throw err;
     var notes = JSON.parse(data);
     let userNote = req.body;
     userNote.id = Math.floor(Math.random() * 5000);
     notes.push(userNote);
-  fs.writeFile('./Develop/db/db.json', JSON.stringify(notes), (err, data) => {
+  fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
       res.json(userNote);
   });
   }); 
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-    fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if (err) throw err;
       let notes = JSON.parse(data);
       const newNotes = notes.filter(note => note.id !== parseInt(req.params.id));
     
-    fs.writeFile('./Develop/db/db.json', JSON.stringify(newNotes), (err, data) => {
+    fs.writeFile('./db/db.json', JSON.stringify(newNotes), (err, data) => {
       res.json({msg: 'successfully'});
     });
   });
@@ -41,7 +41,7 @@ app.delete('/api/notes/:id', (req, res) => {
   });
   
   app.get('/api/notes', (req, res) => {
-    fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if (err) throw err;
       var notes = JSON.parse(data);
       res.json(notes);
